ee2727c6b1e109390b90a4605e45a634e5471066
DustlStar <1776377640@qq.com>
2019-03-28 15:26:53 +0800

    Update Download_Book.py

--- Download_Book.py
+++ Download_Book.py
@@ -15,10 +15,12 @@ from multiprocessing.dummy import Pool
 def get_Urllist(start_url):
     """
     获取每一章链接，存储到一个列表并返回
+    :param html:目录页源代码
     :return:每章链接
     """
-    head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
-    html = requests.get(start_url, headers=head).content.decode('GB2312')
+    # head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
+    # html = requests.get(start_url, headers=head).content.decode('GB2312')
+    html = requests.get(start_url).content.decode('GBK')
     toc_url_list = []
     toc_url = re.findall('href="(\d*.html)"', html, re.S)
     for url in toc_url:
@@ -26,8 +28,13 @@ def get_Urllist(start_url):
     return toc_url_list
 
 def Crawler_save(url):
-    head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
-    html = requests.get(url, headers=head).content.decode('GBK')
+    # head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
+    # html = requests.get(url, headers=head).content.decode('GBK')
+    try:
+        html = requests.get( url , timeout = 60)
+        html = html.content.decode('GBK')
+    except Exception as e:
+        print(e)
     selector = scrapy.Selector(text=html)
     book_name = selector.xpath("//a[@href='index.html']/text()").extract_first()
     chapter_name = selector.xpath("//font[@color='#dc143c']/text()").extract_first()
